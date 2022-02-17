743d13bd1806ca3054e9e5bf6d1d59f557bea014
Lianke Qin <lianke.qin@gmail.com>
2022-02-16 22:03:53 -0800

    add

--- bench.py
+++ bench.py
@@ -1,19 +1,21 @@
+from re import S
 import numpy as np 
 import time
 from scipy.stats import unitary_group
 import scipy.linalg as la
 from scipy.stats import unitary_group
 import math
+import matplotlib.pyplot as plt
 
 class MetricMaintenance:
     def __init__(self, n, d, D):
         self.L = 10 
-        self.m = 10
+        self.m = d
         self.d = d
         self.n = n
         self.D = D #degree of truncation
         self.R = 5 #number of sampled sketches.
-        tmp_sigma = [2 - 1.0/n * i for i in range(self.n)]
+        tmp_sigma = [0.1 for i in range(self.d)]
         self.Sigma = np.zeros((self.d, self.d))
         self.Q = np.zeros((self.d, self.d))
         self.Sigma_sqrt  = np.zeros((self.d, self.d))
@@ -30,12 +32,28 @@ class MetricMaintenance:
         self.fA = np.dot(np.dot(self.Q, self.Sigma_exp), self.Q.T)
 
         self.U = [np.identity(self.d)] #the first element is identity matrix
+        self.A = [np.identity(self.d)] #the first element is identity matrix
+
         for i in range(self.D):
-            self.U.append(np.dot(self.U[-1], self.Sigma_sqrt))
-        
+            self.U.append(np.dot(self.U[-1], self.Sigma_sqrt)) 
+            self.A.append(np.dot(self.A[-1], self.Sigma)) 
+
+        for tau in range(self.D+1):
+            self.U[tau] = np.dot(self.U[tau], 1.0/math.factorial(tau))
+            self.A[tau] = np.dot(self.A[tau], 1.0/math.factorial(tau))
+
+        self.tilde_fA = np.zeros((self.d, self.d))
+        for tau in range(self.D+1):
+            self.tilde_fA += self.A[tau]
+
+
         self.Pi = []
         for i in range(self.L):
-            self.Pi.append(np.random.normal(0, 0.3, (self.m, self.d))) #Pi is the sketch matrix randomly sampled from normal distribution.
+            # tmp = np.zeros((self.m, self.d))
+            # for i in range(self.d):
+            #     tmp[i][i] = np.random.normal(loc=0, scale=math.sqrt(0.1))
+            # self.Pi.append(tmp)
+            self.Pi.append(np.random.normal(loc=0, scale=math.sqrt(1.0/self.m), size=(self.m, self.d))) #TODO Pi is the sketch matrix randomly sampled from normal distribution.
 
         self.x = []
         for i in range(self.n):
@@ -46,6 +64,7 @@ class MetricMaintenance:
             tmp = []
             for tau in range(self.D + 1):
                 tmp.append(np.dot(self.Pi[j], self.U[tau]))
+                #tmp.append(self.U[tau])
             self.Pi_U.append(tmp)
 
         self.tilde_x = []
@@ -58,6 +77,26 @@ class MetricMaintenance:
                     tmp_2.append(tmp)
                 tmp_1.append(tmp_2)
             self.tilde_x.append(tmp_1)
+
+    def diagonal_accuracy(self):
+        tmp = 0
+        for i in range(self.d):
+            diag = 0
+            for j in range(self.D + 1):
+                diag += self.A[j][i][i]
+            tmp += diag/ self.fA[i][i]
+        print(tmp/self.d)
+
+    def test_tilde_A_accuracy(self,q, truncation_degree):
+        acc_result = []
+        tilde_result = []
+        tilde_fA = np.zeros((self.d, self.d))
+        for tau in range(truncation_degree+1):
+            tilde_fA += self.A[tau]
+        for i in range(self.n):
+            acc_result.append(np.dot(np.dot(q-self.x[i], self.fA),(q-self.x[i]).T))
+            tilde_result.append(np.dot(np.dot(q-self.x[i], tilde_fA),(q-self.x[i]).T))
+        return np.array(acc_result), np.array(tilde_result)
 
     def query_one(self, q):
         Pi_U_q = []
@@ -73,7 +112,7 @@ class MetricMaintenance:
             d_i_tau = []
             for r in range(self.R):
                 tmp = Pi_U_q[r][tau] - self.tilde_x[i][r][tau]
-                tmp_norm = np.linalg.norm(tmp)
+                tmp_norm = np.linalg.norm(tmp, ord=2)
                 d_i_tau.append(tmp_norm)
             tilde_d_i_tau = np.median(d_i_tau)
             d_i.append(tilde_d_i_tau)
@@ -105,7 +144,7 @@ class MetricMaintenance:
                     tmp_norm = np.linalg.norm(tmp)
                     d_i_tau.append(tmp_norm)
                 tilde_d_i_tau = np.median(d_i_tau)
-                d_i.append(tilde_d_i_tau * 1.0/math.factorial(tau))
+                d_i.append(tilde_d_i_tau)
             d_i_sum = np.sum(d_i)
             d.append(d_i_sum)
         # print(d)
@@ -113,17 +152,35 @@ class MetricMaintenance:
 
 
 def accuracy(true_result, result):
-    return np.sum(np.abs(true_result - result))/np.sum(np.abs(true_result))
+    # print(true_result)
+    # print(result)
+    return np.mean(result/true_result)
 
 
 
-for D in [1,2,3,5,8,10,20,40,80]:
+
+
+n = 2
+d=100
+q = np.random.rand(d)
+instance = MetricMaintenance(n, d, 40)
+
+for D in [0, 1,3,5,10,20,40]:
     start = time.time()
-    instance = MetricMaintenance(10000, 100, D)
     end = time.time()
     # print("init time {} seconds".format(end-start))
     start = time.time()
-    q = np.random.rand(instance.d)
-    print("D ={} accuracy : {}".format(D, accuracy(instance.query_all_accurate(q), instance.query_all(q))))
+    # instance.diagonal_accuracy()
+    acc_result, tilde_result = instance.test_tilde_A_accuracy(q, D)
+    print("D = {}".format(D))
+    for i in range(n):
+        print("f(A) norm ={}, tilde_f(A) norm={} ".format(acc_result[i], tilde_result[i]))
+    
+    # x = np.linspace(0, n, n)
+    # plt.title("D={}".format(D))
+    # plt.scatter(x, acc_result)
+    # plt.scatter(x, tilde_result)
+    # plt.show()
+    #print("D ={} accuracy : {}".format(D, accuracy(instance.query_all_accurate(q), instance.query_all(q))))
     end = time.time()
     # print("query avg time {} seconds".format(end-start))
