2f586bb55bf846470348c126e12dcc2aff5fe7e9
Lianke Qin <lianke.qin@gmail.com>
2022-02-18 21:22:24 -0800

    add queryone and querypair. TODO support arbitrary f

--- bench.py
+++ bench.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from re import S
 import numpy as np 
 import time
@@ -8,9 +9,10 @@ import math
 import matplotlib.pyplot as plt
 
 class MetricMaintenance:
-    def __init__(self, n, d, D):
+    def __init__(self, n, d, D,m, enableSketch):
+        self.enableSketch = enableSketch
         self.L = 10 
-        self.m = 10
+        self.m = m
         self.d = d
         self.n = n
         self.D = D #degree of truncation
@@ -62,7 +64,10 @@ class MetricMaintenance:
         for j in range(self.L):
             tmp = []
             for tau in range(self.D + 1):
-                tmp.append(np.dot(self.Pi[j], self.U[tau]))
+                if(self.enableSketch):
+                    tmp.append(np.dot(self.Pi[j], self.U[tau]))
+                else:
+                    tmp.append(self.U[tau])
             self.Pi_U.append(tmp)
 
         self.tilde_x = []
@@ -114,15 +119,52 @@ class MetricMaintenance:
             result.append(np.dot(np.dot(q-self.x[i], self.fA),(q-self.x[i]).T))
         return np.array(result)
 
+    def query_one(self, q, i):
+        Pi_U_q = []
+        for r in range(self.R):
+            tmp = []
+            for tau in range(self.D + 1):
+                tmp.append(np.dot(self.Pi_U[r][tau], q))
+            Pi_U_q.append(tmp)
+        d_i = []
+        for tau in range(self.D + 1):
+            d_i_tau = []
+            for r in range(self.R):
+                tmp = Pi_U_q[r][tau] - self.tilde_x[i][r][tau]
+                tmp_norm = 1.0/math.factorial(tau) * np.dot(tmp, tmp.T)
+                d_i_tau.append(tmp_norm)
+            tilde_d_i_tau = d_i_tau[int(self.R/2)] #np.median(d_i_tau)
+            d_i.append(tilde_d_i_tau)
+        d_i_sum = np.sum(d_i)
+        return d_i_sum
+    
+    def query_pair(self, i, j):
+        p = []
+        for tau in range(self.D + 1):
+            d_i_tau = []
+            for r in range(self.R):
+                tmp =  self.tilde_x[i][r][tau] - self.tilde_x[j][r][tau]
+                tmp_norm = 1.0/math.factorial(tau) * np.dot(tmp, tmp.T)
+                d_i_tau.append(tmp_norm)
+            tilde_d_i_tau = d_i_tau[int(self.R/2)] #np.median(d_i_tau)
+            p.append(tilde_d_i_tau)
+        p_sum = np.sum(p)
+        return p_sum
+        return p
+
     def query_all(self, q):
         Pi_U_q = []
+        # start = time.time()
         for r in range(self.R):#TODO lianke, r should be randomly choosen from [L] for R times.
             tmp = []
             for tau in range(self.D + 1):
                 tmp.append(np.dot(self.Pi_U[r][tau], q))
             Pi_U_q.append(tmp)
-        
+        # end = time.time()
+        # print("first part time {}".format(end-start))
         d = []
+
+        # start = time.time()
         for i in range(self.n):
             d_i = []
             for tau in range(self.D + 1):
@@ -131,16 +173,18 @@ class MetricMaintenance:
                     tmp = Pi_U_q[r][tau] - self.tilde_x[i][r][tau]
                     tmp_norm = 1.0/math.factorial(tau) * np.dot(tmp, tmp.T)
                     d_i_tau.append(tmp_norm)
-                tilde_d_i_tau = np.median(d_i_tau)
+                tilde_d_i_tau = d_i_tau[int(self.R/2)] #np.median(d_i_tau)
                 d_i.append(tilde_d_i_tau)
             d_i_sum = np.sum(d_i)
             d.append(d_i_sum)
+        # end = time.time()
+        # print("second part {}".format(end-start))
         return np.array(d)
 
 
 def accuracy(true_result, result):
 
-    return np.mean(abs(result - true_result)/true_result)
+    return 1 - np.mean(abs(result - true_result)/true_result)
 
 
 
@@ -148,19 +192,55 @@ def accuracy(true_result, result):
 
 n=10000
 
-for d in [100, 1000, 5000, 10000]:
-    for D in [0, 1,2,3,4, 5,10,20,40]:
-        q = np.random.rand(d)
-        instance = MetricMaintenance(n, d, D)
-        # print("d={} D ={} init time {} seconds".format(d, D, end-start))
-        start = time.time()
-        print("d={} D ={} error rate : {}".format(d, D, accuracy(instance.query_all_accurate(q), instance.query_all(q))))
-        end = time.time()
-        # print("d={} D ={}query avg time {} seconds".format(d, D,end-start))
+init_time = [1.1681427955627441, 1.5417754650115967, 5.062142848968506, 16.5169997215271, 24.563014268875122]
+query_time = [0.4315241813659668, 0.44232289791107177, 0.46778163909912107, 0.4692366123199463, 0.5201902389526367]
+accuracy_list = [0.6919568568103092, 0.7497045783016956, 0.8461419165704719, 0.8918277703205306, 0.9221402268148761]
+
+# for d in [1000]:
+#     for m in [10, 20, 40, 80, 160]:
+#         for D in [3]:
+#             start = time.time()
+#             instance = MetricMaintenance(n, d, D, m, True)
+#             end = time.time()
+#             init_time.append(end - start)
+#             print("d={} sketch_size = {} D ={} \ninit time {} seconds".format(d, m, D, end-start))
+#             start = time.time()
+#             for i in range(10):
+#                 q = np.random.rand(d)
+#                 instance.query_all(q)
+#             end = time.time()
+#             query_time.append((end-start)/10)
+#             print("query all time {} seconds".format((end-start)/10))
+#             accuracy_list.append(accuracy(instance.query_all_accurate(q), instance.query_all(q)))
+#             # print("d={} D ={}query avg time {} seconds".format(d, D,end-start))
+
+print(init_time)
+print(query_time)
+print(accuracy_list)
+
+ticks_size = 20
+
+x = np.linspace(0, len(init_time), len(init_time))
+plt.plot(x, init_time, label='init time')
+plt.plot(x, query_time, label='query time')
+plt.xlabel("sketch size", fontsize= ticks_size)
+plt.ylabel("time (sec)", fontsize= ticks_size)
+plt.xticks(x, [10, 20, 40, 80, 160], fontsize= ticks_size)
+plt.legend(loc='upper left')
+plt.yticks(fontsize=ticks_size)
+plt.legend(loc="best", fontsize=20)
+plt.savefig("time_sketch_size.pdf", dpi=None, facecolor='w', edgecolor='w',
+            orientation='portrait', bbox_inches='tight')
+plt.show()
 
 
-# acc_result, tilde_result, summation_tilde_result = instance.test_tilde_A_accuracy(q, D)
-# print("D = {}".format(D))
-# for i in range(n):
-#     print("f(A) norm ={}, tilde_f(A) norm={}, summation = {} ".format(acc_result[i], tilde_result[i], summation_tilde_result[i]))
-        
+plt.plot(x, accuracy_list)
+plt.xlabel("sketch size", fontsize= ticks_size)
+plt.ylabel("accuracy", fontsize= ticks_size)
+plt.xticks(x, [10, 20, 40, 80, 160], fontsize= ticks_size)
+plt.yticks(fontsize=ticks_size)
+plt.savefig("accuracy_sketch_size.pdf", dpi=None, facecolor='w', edgecolor='w',
+            orientation='portrait', bbox_inches='tight')
+plt.show()
+
+
---
+++ function_plot.py
@@ -0,0 +1,30 @@
+from cProfile import label
+from re import S
+import numpy as np 
+import time
+from scipy.stats import unitary_group
+import scipy.linalg as la
+from scipy.stats import unitary_group
+import math
+import matplotlib.pyplot as plt
+
+x = np.linspace(-4,4,100)
+y_exp = np.exp(x)
+y_exp_minus = np.exp(-x)
+y_cosh = np.cosh(x)
+y_sinh = np.sinh(x)
+
+ticks_size = 20
+plt.plot(x, y_exp, label='exp(x)')
+plt.plot(x, y_exp_minus, label='exp(-x)')
+plt.plot(x, y_cosh, label='cosh(x)')
+plt.plot(x, y_sinh, label='sinh(x)')
+plt.xlabel("x", fontsize= ticks_size)
+plt.ylabel("y", fontsize= ticks_size)
+plt.xticks(fontsize= 15)
+plt.yticks(fontsize= 15)
+plt.legend(loc='best')
+plt.legend(loc="best", fontsize=15)
+plt.savefig("functions_figure.pdf", dpi=300, facecolor='w', edgecolor='w',
+            orientation='portrait', bbox_inches='tight')
+plt.show()
